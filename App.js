4de8048b1d615959b56b01a41915a1fbd9b85ba2
Shyam-25 <shyam.s@optisolbusiness.com>
2020-11-24 17:29:19 +0530

    firstcommit

--- App.js
+++ App.js
@@ -1,17 +1,55 @@
+import React from 'react';
+import { SafeAreaView, Image,ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
-import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-
-const UselessTextInput = () => {
-  const [value, onChangeText] = React.useState('Useless Placeholder');
-
+const YourApp = () => {
   return (
-    <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText(text)}
-      value={value}
-    />
+    <SafeAreaView style={styles.safe}>
+      <ScrollView style={styles.scrollView}>
+    <View
+        style={{
+         flex:4,
+          padding: 10
+        }}>
+        <View style={styles.container} />
+        <View style={{ backgroundColor: "pink",flex:4,height:1000, }} />
+       <View style={styles.viewimage}><Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}/></View>
+        <Text style={styles.textarea}>EnterUser Name</Text>
+        <TextInput style={styles.input}/>
+      </View>
+      </ScrollView>
+      </SafeAreaView>
+     
   );
 }
 
-export default UselessTextInput;
+const styles= StyleSheet.create({
+tinyLogo:{
+  width:60,
+  height:60,
+  alignItems:'center',
+},
+viewimage:{
+  alignItems:'center',
+},
+container:{
+backgroundColor:"red",
+height:500,
+borderColor:'blue',
+borderWidth:4,
+},
+textarea:{
+borderColor:"gray",
+borderWidth:2,
+margin:8,
+},
+input:{
+  borderColor:'gey',
+  borderWidth:3,
+},
+});
+
+export default YourApp;
