e5b2db219fe07e5eb8237f43e91d7c17c041d799
Shyam-25 <shyam.s@optisolbusiness.com>
2020-11-25 14:57:03 +0530

    firstcommit

--- App.js
+++ App.js
@@ -1,55 +1,96 @@
-import React from 'react';
-import { SafeAreaView, Image,ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { Component } from 'react';
+import {StyleSheet,Text,View,TextInput,TouchableHighlight,Scrollview,Image,Alert } from 'react-native';
 
-const YourApp = () => {
-  return (
-    <SafeAreaView style={styles.safe}>
-      <ScrollView style={styles.scrollView}>
-    <View
-        style={{
-         flex:4,
-          padding: 10
-        }}>
-        <View style={styles.container} />
-        <View style={{ backgroundColor: "pink",flex:4,height:1000, }} />
-       <View style={styles.viewimage}><Image
-        style={styles.tinyLogo}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}/></View>
-        <Text style={styles.textarea}>EnterUser Name</Text>
-        <TextInput style={styles.input}/>
+
+export default class LoginView extends Component {
+
+  constructor(props) {
+    super(props);
+    state = {
+      email   : '',
+      password: '',
+    }
+  }
+
+  onClickListener = (viewId) => {
+    Alert.alert("Alert", "Button pressed "+viewId);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} />
+          <TextInput style={styles.inputs}
+              placeholder="Email"
+              keyboardType="email-address"
+              onChangeText={(email) => this.setState({email})}/>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} />
+          <TextInput style={styles.inputs}
+              placeholder="Password"
+              secureTextEntry={true}
+              onChangeText={(password) => this.setState({password})}/>
+        </View>
+
+        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('login')}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableHighlight>
+
+
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
+            <Text> New user..?click here to Register</Text>
+        </TouchableHighlight>
       </View>
-      </ScrollView>
-      </SafeAreaView>
-     
-  );
+    );
+  }
 }
 
-const styles= StyleSheet.create({
-tinyLogo:{
-  width:60,
-  height:60,
-  alignItems:'center',
-},
-viewimage:{
-  alignItems:'center',
-},
-container:{
-backgroundColor:"red",
-height:500,
-borderColor:'blue',
-borderWidth:4,
-},
-textarea:{
-borderColor:"gray",
-borderWidth:2,
-margin:8,
-},
-input:{
-  borderColor:'gey',
-  borderWidth:3,
-},
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FA8072',
+  },
+  inputContainer: {
+      borderBottomColor: '#808080',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:46,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: "#000000",
+  },
+  loginText: {
+    color: 'gray',
+  }
 });
-
-export default YourApp;
