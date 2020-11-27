2ecc627cf6c85c80025e1354360a772b33f5bbb2
Shyam-25 <shyam.s@optisolbusiness.com>
2020-11-27 15:58:57 +0530

    switchcommit

--- App.js
+++ App.js
@@ -1,48 +1,23 @@
 import React from 'react';
-import { View, KeyboardAvoidingView, TextInput, StyleSheet, Text, Platform, TouchableWithoutFeedback, Button, Keyboard  } from 'react-native';
+import { Text, View, Stylesheet, Switch} from 'react-native';
 
-const Example = () => {
-  return (
-    <KeyboardAvoidingView
-      behavior={Platform.android == "android" ? "padding" : "height"}
-      style={styles.container}
-    >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.inner}>
-          <Text style={styles.header}>Welcome</Text>
-          <TextInput placeholder="Username" style={styles.textInput} />
-          <View style={styles.btnContainer}>
-            <Button title="Submit" onPress={() => null} />
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+export default function App(){
+  return(
+    <View style={Stylesheet.container}>
+      <Text style={Stylesheet.Text}>Switch</Text>
+      <Switch value={true}/>
+    </View>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  inner: {
-    padding: 24,
-    flex: 1,
-    justifyContent: "space-around"
-  },
-  header: {
-    fontSize: 36,
-    marginBottom: 48
-  },
-  textInput: {
-    height: 50,
-    borderColor: "#000000",
-    borderBottomWidth: 1,
-    marginBottom: 36
-  },
-  btnContainer: {
-    backgroundColor: "white",
-    marginTop: 12
-  }
+}
+const styles = Stylesheet.create({
+  container:{
+  flex: 1,
+  backgroundColor: '#000000',
+  alignItems:'center',
+  justfyContent:'center',
+},
+Text:{
+  fontSize: 10,
+  color:"#161616"
+}
 });
-
-export default Example;
