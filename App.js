676831838ba80b01ebfe6f1eb8ab8654016dd042
Shyam-25 <shyam.s@optisolbusiness.com>
2020-11-27 10:21:58 +0530

    button

--- App.js
+++ App.js
@@ -1,96 +1,27 @@
-import React, { Component } from 'react';
-import {StyleSheet,Text,View,TextInput,TouchableHighlight,Scrollview,Image,Alert } from 'react-native';
+import React,{Component} from 'react';
+import {Text,View,Button} from 'react-native';
 
+export default class App extends Component
+{
+   Task1=()=>
+   {
+     alert("Welcome to Chennai");
+   }
 
-export default class LoginView extends Component {
+   Task2=(Num)=>
+   {
+     alert(Num);
+   }
 
-  constructor(props) {
-    super(props);
-    state = {
-      email   : '',
-      password: '',
-    }
-  }
+  render()
+  {
+    return(
+      <View>
+        <Button title="Button1" onPress={this.Task1}></Button>
 
-  onClickListener = (viewId) => {
-    Alert.alert("Alert", "Button pressed "+viewId);
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-          <Image style={styles.inputIcon} source={{uri: 'https://reactnative.dev/img/tiny_logo.png',}}/>
-          <Image style={styles.inputIcon} />
-          <TextInput style={styles.inputs}
-              placeholder="Email"
-              keyboardType="email-address"
-              onChangeText={(email) => this.setState({email})}/>
-        </View>
-        
-        <View style={styles.inputContainer}>
-          <Image style={styles.inputIcon} />
-          <TextInput style={styles.inputs}
-              placeholder="Password"
-              secureTextEntry={true}
-              onChangeText={(password) => this.setState({password})}/>
-        </View>
-
-        <TouchableHighlight style={[styles.buttonContainer]} onPress={() => this.onClickListener('login')}>
-          <Text>Login hi</Text>
-        </TouchableHighlight>
-
-
-        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
-            <Text> New user..?click here to Register</Text>
-        </TouchableHighlight>
+        <Button title="Button2" onPress={this.Task2.bind(this,100)}></Button>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FA8072',
-  },
-  inputContainer: {
-      borderBottomColor: '#808080',
-      backgroundColor: '#FFFFFF',
-      borderRadius:30,
-      borderBottomWidth: 1,
-      width:250,
-      height:45,
-      marginBottom:20,
-      flexDirection: 'row',
-      alignItems:'center'
-  },
-  inputs:{
-      height:45,
-      marginLeft:46,
-      borderBottomColor: '#FFFFFF',
-      flex:1,
-  },
-  inputIcon:{
-    width:30,
-    height:30,
-    marginLeft:15,
-    justifyContent: 'center'
-  },
-  buttonContainer: {
-    height:45,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom:20,
-    width:250,
-    borderRadius:30,
-  },
-  loginButton: {
-    backgroundColor: "#000000",
-  },
-  loginText: {
-    color: 'gray',
-  }
-});
+       
