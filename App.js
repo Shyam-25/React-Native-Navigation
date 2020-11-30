68fcc8879431dab587e913281103a2bd7a57907a
Shyam-25 <shyam.s@optisolbusiness.com>
2020-11-30 18:19:58 +0530

    second commit

--- App.js
+++ App.js
@@ -1,47 +1,111 @@
-import React from 'react';
-import { Stylesheet, Text, View, Flatlist } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert
+} from 'react-native';
 
-export default function App() {
-  const [people, setPeople] = useState([
-        { name: 'Ashwin', id:'1' },
-        { name: 'Shyam', id:'2'},
-        { name: 'Sundar', id:'3'},
-        { name: 'Ramesh', id:'4'},
-        { name: 'jai', id:'5'},
-        { name: 'kumar', id:'6'},
-  ]);
+export default class LoginView extends Component {
 
-  return(
-    <View style={styles.container}>
-    
-    <Flatlist
-       numColumns={2}
-       keyExtractor={(item) => item.id}
-       data={people}
-       renderItem={({ item })=>(
-         <Text style={styles.item}>{item.name}</Text>
-       )}
-    />
-
-    </View>
-
-
-  );
-}
-                         
-const styles = Stylesheet.create({
-  container: {
-       flex:1,
-       backgroundColor: '#fff',
-       paddingTop: 40,
-       paddingHorizontal: 20
-  },
-  item:{
-    marginTop: 24,
-    padding: 30,
-    backgroundcolor: 'pink',
-    fontSize: 25,
-    marginHorizontal: 10;
-    marginTop: 24;
+  constructor(props) {
+    super(props);
+    state = {
+      email   : '',
+      password: '',
+    }
   }
-})
+
+  onClickListener = (viewId) => {
+    Alert.alert("Alert", "Button pressed "+viewId);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Image style={styles.inputIcon} source={{uri: 'https://reactnative.dev/img/tiny_logo.png',}}/>
+              <Text style={{fontSize:40,color:'grey', paddingTop:10}}>SIGN IN</Text>
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon}/> 
+          <TextInput style={styles.inputs}
+              placeholder="Email"
+              keyboardType="email-address"
+              underlineColorAndroid='transparent'
+              onChangeText={(email) => this.setState({email})}/>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon}/> 
+          <TextInput style={styles.inputs}
+              placeholder="Password"
+              secureTextEntry={true}
+              underlineColorAndroid='transparent'
+              onChangeText={(password) => this.setState({password})}/>
+        </View>
+
+        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('login')}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
+            <Text>Forgot your password?</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
+            <Text>Register</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+  },
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  inputIcon:{
+    width:50,
+    height:50,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: "#00b5ec",
+  },
+  loginText: {
+    color: 'white',
+  }
+});
