513dbd6eac18373f032398dc3d74b683cae79282
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-01 15:06:06 +0530

    first commit

--- App.js
+++ App.js
@@ -1,65 +1,53 @@
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, SectionList } from "react-native";
+import React from 'react';
+import {
+  ScrollView,
+  RefreshControl,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+} from 'react-native';
 
 
+const wait = (timeout) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
+}
 
-const DATA = [
-  {
-    title: "Food",
-    data: ["Meals", "Dosa", "Idly"]
-  },
-  {
-    title: "Ice Cream",
-    data: ["Chocolate", "Grape-Nut", "Blue-Moon"]
-  },
-  {
-    title: "Vegetable",
-    data: ["Tomato", "Carrot", "Beans"]
-  },
-  {
-    title: "Drinks",
-    data: ["Water", "Coke", "Fanta"]
-  }
-];
+const App = () => {
+  const [refreshing, setRefreshing] = React.useState(false);
 
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
 
-const App = () => (
-  <SafeAreaView style={styles.container}>
-    <SectionList
-      sections={DATA}
-      keyExtractor={(item, index) => item + index}
-      renderItem={({ item }) => <Item title={item} />}
-      renderSectionHeader={({ section: { title } }) => (
-        <Text style={styles.header}>{title}</Text>
-      )}
-    />
-  </SafeAreaView>
-);
+    wait(600).then(() => setRefreshing(false));
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        <Text>Pull down to see Refresh</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 20,
-    marginHorizontal: 16
   },
-  item: {
-    backgroundColor: "#f9c2ff",
-    padding: 20,
-    marginVertical: 8
+  scrollView: {
+    flex: 1,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  header: {
-    fontSize: 32,
-    backgroundColor: "#000",
-    backgroundColor: "#a52a2a"
-  },
-  title: {
-    fontSize: 22
-  }
 });
 
 export default App;
