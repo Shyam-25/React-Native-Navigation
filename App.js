a7bc72c160b7540321e5426f4cbb0473599de634
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-01 17:12:22 +0530

    firstcommit

--- App.js
+++ App.js
@@ -1,38 +1,59 @@
 import React from 'react';
-import {
-  ScrollView,
-  RefreshControl,
-  StyleSheet,
-  Text,
-  SafeAreaView,
-} from 'react-native';
+import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
 
+const DATA = [
+  {
+    id: '1',
+    title: 'Cricket',
+  },
+  {
+    id: '2',
+    title: 'Football',
+  },
+  {
+    id: '3',
+    title: 'Volleyball',
+  },
+  {
+    id: '4',
+    title: 'baseball',
+  },
+  {
+    id: '5',
+    title: 'kabadi',
+  },
+  {
+    id: '6',
+    title: 'Hockey',
+  },
+  {
+    id: '7',
+    title: 'chess',
+  },
+  {
+    id: '8',
+    title: 'boxing',
+  }
+];
 
-const wait = (timeout) => {
-  return new Promise(resolve => {
-    setTimeout(resolve, timeout);
-  });
-}
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const App = () => {
-  const [refreshing, setRefreshing] = React.useState(false);
-
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-
-    wait(600).then(() => setRefreshing(false));
-  }, []);
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView
-        contentContainerStyle={styles.scrollView}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      >
-        <Text>Pull down to see Refresh</Text>
-      </ScrollView>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </SafeAreaView>
   );
 }
@@ -40,13 +61,18 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
+    marginTop: 10,
+    backgroundColor: 'grey'
   },
-  scrollView: {
-    flex: 1,
-    backgroundColor: 'grey',
-    alignItems: 'center',
-    justifyContent: 'center',
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 18,
+    marginHorizontal: 26,
+  },
+  title: {
+    fontSize: 32
+    
   },
 });
 
