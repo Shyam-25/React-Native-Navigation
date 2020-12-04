6d6ccdc2000ff3d810a1cdeaa4dffdb812a7583a
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-04 12:04:54 +0530

    firstreact

--- App.js
+++ App.js
@@ -1,211 +1,122 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet,Image , RefreshControl } from 'react-native';
+import { Text, View, SectionList, Stylesheet } from 'react-native';
+
+const year2019=[
+  {
+    id:'1',
+    value:'An event with a single day',
+    days:'0 days',
+    month:'Nov 3,2019'
+  },
+  {
+    id:'2',
+    value:'An Ongoing Event',
+    days:'10 days',
+    month:'Jan 1,2019',
+    content:'.Ongoing'
+  }
+];
+
+const year2018=[
+  {
+    id:'1',
+    value:'An event with a data range',
+    days:'1yr,3mnth,27 days',
+    month:'Jun 3,2016',
+    content:'. Jun8, 2018'
+  }
+];
+
+const year2016=[
+  {
+    id:'1',
+    value:'An event with a data range',
+    days:'3yr,5mnth,1day',
+    month:'Jun 3,2016',
+    content:'. Jun 8,2018'
+  }
+];
+
+const DATA= [
+  {
+    title:'2019',
+    data:year2019
+  },
+  {
+    title:'2018',
+    data:year2018
+  },
+  {
+    title:'2016',
+    data:year2016
+  },
+];
+
+SectionListItems = ({ items }) =>(
+  <View styles={styles.container}>
+    <Text style={{fontSize:40,color:'grey', paddingTop:10,fontWeight:'bold'}}>Test Milestones</Text>
+
+    <View style={styles.inputcontainer}>
+      <Text style={styles.text}>{items.value}</Text>
+      <Text style={styles.days}>{items.days}</Text>
+      <Text style={styles.month}>{items.month}</Text>
+      <Text style={styles.content}>{items.content}</Text>
+
+      
+    </View>
+  </View>
+);
 
 export default class App extends React.Component {
-    state = {
-        firstdata:[
-      {
-          id:'1',
-          name:'Virat kohli',
-          content:'Virat Kohli is an Indian cricketer and the current captain of the India national team',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          time:'11.30 AM',
-         },
-      {
-          id:'2',
-          name:'Vijay',
-          content:'Vijay is an Indian actor, dancer, playback singer and philanthropist who works in Tamil cinema',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          time:'12.30 PM',
-        },
-      {
-          id:'3',
-          name:'Sunil Chhetri',
-          content:'Sunil Chhetri is an Indian professional footballer who plays as a striker and captains both the ISL and the Indian team.',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          time:'1.30 PM',
-        },
-      {
-          id:'4',
-          name:'Anirudh',
-          content:'Anirudh is an Indian film composer and singer who primarily works in Tamil cinema',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          time:'2.30 PM',
-        },
-      {
-          id:'5',
-          name:'MS Dhoni',
-          content: 'Dhoni is a former Indian international cricketer who captained the Indian national team in limited-overs formats',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          time:'3.30 PM',
-        },
-      {
-          id:'6',
-          name:'Ravi Ashwin',
-          content:' Ashwin is an Indian international cricketer. An all-rounder who bats right-handed and bowls right-arm off break',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          hours:'4.30 PM',
-        },
-    ],
-        Seconddata: [
-      {
-          id:'7',
-          name:'Sam curran',
-          content:' Samuel Matthew Curran is an English cricketer, who plays for Surrey, and England.',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          hours:'5.30 PM',
-
-         },
-         {
-          id:'8',
-          name:'Shyam',
-          content:' Shyam is Good boy and he works well, he plays cricket',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          hours:'6.30 PM',
-         },
-         {
-          id:'9',
-          name:'Arun',
-          content:' Arun is Good boy and he works well,He play kabadi',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          hours:'7.30 PM',
-
-         },
-         {
-          id:'10',
-          name:'Bala',
-          content:'Bala shares his birthday with the legend Wasim Akram, but he has also shown the same qualities with the ball at a very young age',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          hours:'8.30 PM',
-         },
-         {
-          id:'11',
-          name:'Ramesh',
-          content:'Indian film actor and producer who works in Tamil and Telugu cinema',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          hours:'9.30 PM',
-         },
-         {
-          id:'12',
-          name:'Jeeva',
-          content:'best known by his stage name Jithan Ramesh, is an Indian actor',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          hours:'10.30 PM',
-         },
-         {
-          id:'13',
-          name:'Rio',
-          content:'hsybxy osouie udtcsu dsudhcodi sudhbcolsc slublsuhoguoidh yesvfd dsludeu lbsldhfsdkn',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          hours:'11.30 PM',
-         },
-         {
-          id:'14',
-          name:'kumar',
-          content:'wrwsvsh egdsksgerty gdsklpwbh kdgeytrkb hdblkdhbc iduhcdjcn hudsiblncl oidccnd akjdbkn',
-          Uri:'https://reactnative.dev/img/tiny_logo.png',
-          hours:'12.30 AM',
-         },
-    ],
-    isRefreshing:false,
-    arr:[]
- }
- renderRow = ({ item }) => {
-    return (
-      <View style={styles.container}>
-        <View style={styles.imageHolder}>
-            <Image source={{uri:item.Uri}} style={styles.imageContainer}/>   
-        </View>
-        <View style={styles.textContainer}>
-            <Text style={styles.title}>{item.name}</Text>
-            <Text style={styles.content}>{item.content}</Text>
-            <Text style={styles.time}>{item.time}</Text>
-            
-        </View>
-      </View>
-    )
-  }
-  onendReached=()=>{         
-    console.log(this.state.firstdata.length)
-    if(this.state.firstdata.length<this.state.Seconddata.length+5){
-      this.setState({
-          firstdata:this.state.firstdata.concat(this.state.Seconddata.slice(this.state.firstdata.length-6,this.state.firstdata.length-1))
-        })
-      } 
-  }
-  
-  refresh=()=>{
-    this.setState({isRefreshing:true})
-  }
-
+      
   render() {
     return (
       <View >
-        <FlatList
-          data={this.state.firstdata}
-          renderItem={this.renderRow}
-          keyExtractor={item=>item.id}
-          extraData={this.state.Seconddata}
-          onEndReached={this.onendReached}
-          onEndReachedThreshold={0}
-          refreshControl={
-            <RefreshControl refreshing={this.state.isRefreshing} onRefresh={this.refresh}/>
-          }
-        />
+        <SectionList
+          sections={DATA}
+          renderItem={({item})=><SectionListItems items={item}/>}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text style={styles.header}>{title}</Text>
+          )}
+         />
       </View>
     )
   }
 }
 
-const styles=StyleSheet.create({
+const styles=Stylesheet.create({
+  header:{
+    paddingLeft:10,
+    fontSize:14,
+    paddingBottom:5,
+    fontWeight:'bold',
+  },
   container:{
     flexDirection:'row',
+    backgroundColor:'#ffffff',
   },
-  imageHolder:{
-    padding:10,
-    alignItems:'center',
-   backgroundColor:'pink',
-    borderBottomWidth:5,
-    borderBottomColor:'grey',
-   
-  },
-  imageContainer:{
-    width:50, 
-    height:50,
-    padding:10,
-    borderRadius:50,
-    marginTop:10,
-  },
- 
-  textContainer:{
+  inputcontainer:{
+    paddingTop:20,
+    paddingBottom:20,
+    paddingRight:20,
     paddingLeft:10,
-    paddingRight:100,
-    borderBottomColor:'white',
-    borderBottomWidth:5,
-    backgroundColor:'white',
-    flexDirection:'column',
-    fontStyle: 'italic',
-    backgroundColor:'grey'
-    
- },
-  title:{ 
-    paddingTop:.5,
-    color:'#00b0ff',
-    fontSize:30,
-    fontWeight: "bold",
-    justifyContent:'flex-start'
-    
+    flexDirection:'row',
+    backgroundColor:'#00F'
+  },
+  text:{
+    fontsize:'20',
+    fontWeight:'bold'
+
+  },
+  days:{
+    fontsize:'10',
+
+  },
+  month:{
+    fontsize:'10',
   },
   content:{
-    paddingBottom:10,
-    justifyContent:'space-evenly',
-    fontSize:15 ,
-    backgroundColor:'white',
-    fontStyle: 'italic',
-    
+    fontsize:'10'
   },
-  time:{
-    paddingBottom:15,
-    color:'white',
-  },
+
 })
