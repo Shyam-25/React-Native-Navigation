a425f491e48c3e63fd7679c58d2cd12e5435adc9
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-07 16:45:37 +0530

    firstcommit

--- App.js
+++ App.js
@@ -1,36 +1,82 @@
-import React from 'react';
-import { View, Text , Modal , Button } from 'react-native';
+import React, {useState}  from "react";
+import {
+  Alert, Modal, StyleSheet, Text, TouchableHighlight, View } from "react-native";
 
-class App extends React.Component {
-  constructor ()
-  {
-    super();
-    this.state={
-      show:false
-    }
-  }
 
-  render () {
-    return(
-      <View style= {{ flex:1, marginTop: 80 }}>
-        <Text style= {{ fontSize: 100 , fontWeight: 'bold'}}>Hello Modal</Text>
-        <Button title="open modal" onPress={()=>{this.setState({show:true})}}  />
-        <Modal
+const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View style={styles.container}>
+    <Text style={ {fontSize: 30, fontWeight:"bold", backgroundColor: 'white' } }>Welcome to Modal</Text>
+      <Modal
+        animationType="slide"
         transparent={true}
-        visible={this.state.show}
-         >
-         <View style={{ backgroundColor:'#000000aa',flex:1}}>
-         <View style={{ backgroundColor:'#fff', margin:50, padding:40, borderRadius: 10, flex:1 }}>
-         <Text style={{ fontSize: 50 }}>Welcome to React-Native</Text>
-         <Button title="Close modal" onPress={()=>{this.setState({show:false})}}  />
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.container}>
+          <View style={styles.inputcontainer}>
+            <Text style={styles.modalText}>Hello Modal</Text>
 
-         </View>
-        
-         </View>
-         
-        </Modal>
-      </View>
-    )
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={styles.textStyle}>Close !</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+      <TouchableHighlight
+        style={styles.openButton}
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      >
+        <Text style={styles.textStyle}>Open Modal </Text>
+      </TouchableHighlight>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    height: 70,
+    backgroundColor: 'grey'
+  },
+  inputcontainer: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 12,
+    
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
-}
+});
+
 export default App;
