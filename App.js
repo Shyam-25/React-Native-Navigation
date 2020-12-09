cc14b8b36c439c4e106519ffca94f09fce616125
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-09 18:35:03 +0530

    firstcommit

--- App.js
+++ App.js
@@ -1,32 +1,55 @@
-import React from 'react';
-import { View, Picker } from 'react-native';
+import React, { Component } from "react";
+import {View,Text, StyleSheet,Button,Alert} from "react-native";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-class App extends React.Component {
-  constructor()
-  {
-    super();
-    this.state={selectedLable:''};
-  }
 
-  show=(value)=>
-  {
-    alert(value);
-    this.setState({selectedLable:value});
-  }
-  
-  render () {
-    return(
-      <View>
-     <Picker 
-          selectedValue={this.state.selectedLable}
-          onValueChange={this.show.bind()} >
-       <Picker.Item label='Select a course' value='0' color="#FF0000"></Picker.Item>
-       <Picker.Item label='React' value='5000'></Picker.Item>
-       <Picker.Item label='Javascript' value='4000'></Picker.Item>
-       <Picker.Item label='OOPS' value='3000'></Picker.Item>
-     </Picker>
-     </View>
-    );
-  }
+
+class SimplePicker extends Component{
+
+state={
+  DATE:new Date(),
+  touched:false
 }
-export default App;
+onHandeler=(date)=>{
+ this.setState({DATE:date})
+  console.log(this.state.DATE)
+}
+onPressed=()=>{
+this.setState({touched:true})
+}
+
+
+render (){
+  return(
+    <View style={styles.container}>
+    
+      <Button title ='Date' onPress={this.onPressed}/> 
+        {this.state.touched&&(<DateTimePicker
+        mode='date'
+        maximumDate={new Date(2025,4,20)}
+        minimumDate={new Date(2019,1,12)}
+        value={this.state.DATE}
+        onChange={this.onHandeler}
+        is24Hour={true}
+        />)}
+        
+    </View>
+    
+  )
+  
+}
+}
+
+const styles= StyleSheet.create({
+  container:{
+padding:80,
+  },
+  pickerElement:{
+    width:150,
+    borderColor:'black',
+    borderWidth:2,
+    height:40,
+  },
+
+});
+export default SimplePicker;
