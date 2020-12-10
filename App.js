7c92ab5d353b8828a8c58031e3a59d6300e047eb
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-10 15:09:25 +0530

    pickercommit

--- App.js
+++ App.js
@@ -1,23 +1,69 @@
 import React, { Component } from "react";
-import {View , Alert} from "react-native";
-import { CustomPicker } from 'react-native-custom-picker'
+import {View,Text,Image, StyleSheet,Button,Alert, TouchableOpacity} from "react-native";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import ImagePicker from 'react-native-image-picker'
 
-class Picker extends Component{
-render (){
-  const options = ['C', 'C++', 'Java', 'React-native', 'Javascript'] 
-  return(
-
-      <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
-        <CustomPicker
-        options={options}
-        onValueChange={value => {
-          Alert.alert('Selected course', value || 'No course are selected!')
-        }}/>
-    </View>
+const options={
+  title:'My Gallery',
+  takePhotoButtonTitle:'Take photo with your camera',
+  chooseFromLibraryButtonTitle:'choose photo from gallery',
+}
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      imageSource:null
+    }
+  }
+  
+    myfun() => {
     
-  )
-}
-}
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+    
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else {
+        const source = { uri: response.uri };
+        this.setState({
+          imageSource: source,
+        });
+      }
+    });
+  }
+  }
 
 
-export default Picker;
+  render () {
+    return (
+      <View styles={styles.container}>
+        <Text style={styles.welcome}>Welcome to React native</Text>
+
+        <Image source={this.state.imageSource}
+        style={{width:100,height:300,margin:10}}></Image>
+
+
+        <TouchableOpacity style={{backgroundColor:'green',margin:10,padding:10}}>
+        
+          <Text style={{color:'#fff'}}>Select Image</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+  
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#fff'
+  },
+  welcome:{
+    fontSize:20,
+    textAlign:'center'
+  }
+
+}); 
