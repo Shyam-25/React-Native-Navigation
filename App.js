0fd2cdbfaa3b0926e370bf5f43d7764bc5894a01
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-17 17:46:03 +0530

    axios

--- App.js
+++ App.js
@@ -1,53 +1,46 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Text, View } from 'react-native';
+import axios from 'axios';
 
-export default class App extends React.Component {
+class App extends React.Component {
+
+  constructor()
+  {
+    super ()
+    this.state = {
+      data:[]
+    }
+  }
+
+  componentDidMount()
+  {
+    this.getapiData()
+  }
+  async getapiData()
+  {
+    let resp=await axios.get('https://reactnative.dev/movies.json')
+
+    console.warn(resp.data.movies)
+    this.setState({data: resp.data.movies })
+  }
   render() {
     return (
-      <View style={styles.container}>
-      <TouchableOpacity onPress={this.saveData}>
-        <Text style={{fontSize:25}}>Save Data</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={this.displayData}>
-        <Text style={{fontSize:25}}>Display Data</Text>
-        </TouchableOpacity>
-
+      <View style={ { flex:1, margin: 70 }} >
+      {
+        this.state.length>0?
+        <View>
+        {
+              this.state.data.map((item)=>
+              <Text style={{fontSize:40}}>{item.title},{item.releaseYear}</Text>)
+        }
+        </View> : <Text style={{fontSize:40}}> data is loading.. </Text>
+        
+                   
+      }
+        
       </View>
-    );
-  }
-  saveData() {
-    let obj = {
-      name: 'Shyam Sundar',
-      City: 'Chennai',
-      department: 'Mobility'
-    }
-    AsyncStorage.setItem('user', JSON.stringify(obj));
-    
-  } 
-  displayData = async () => {
-    try {
-      let user = await AsyncStorage.getItem('User');
-      let parsed = JSON.parse(user);
-      alert(parsed.City);
-
-    }
-
-    catch(error) {
-      alert(error);
-
-    }
+    )
   }
 }
 
-
-
-const styles = StyleSheet.create ({
-  container: {
-    flex:1,
-    backgroundColor: 'pink',
-    alignItems : 'center',
-    justifyContent: 'center'
-  },
-});
+export default App;
