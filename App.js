cf804b0afb02e0570f6060844682161b2568a09e
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-18 11:09:35 +0530

    redux-2

--- App.js
+++ App.js
@@ -1,46 +1,24 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import axios from 'axios';
+import React from 'react'
+import { Text, View } from 'react-native'
+import axios from 'axios'
 
 class App extends React.Component {
 
-  constructor()
+  componentWillUnmount()
   {
-    super ()
-    this.state = {
-      data:[]
-    }
+    axios.get('https://reactnative.dev/movies.json')
+  .then(function (response) {
+    console.warn(response.data.movies);
+   })
+  
   }
 
-  componentDidMount()
-  {
-    this.getapiData()
-  }
-  async getapiData()
-  {
-    let resp=await axios.get('https://reactnative.dev/movies.json')
-
-    console.warn(resp.data.movies)
-    this.setState({data: resp.data.movies })
-  }
-  render() {
-    return (
-      <View style={ { flex:1, margin: 70 }} >
-      {
-        this.state.length>0?
-        <View>
-        {
-              this.state.data.map((item)=>
-              <Text style={{fontSize:40}}>{item.title},{item.releaseYear}</Text>)
-        }
-        </View> : <Text style={{fontSize:40}}> data is loading.. </Text>
-        
-                   
-      }
-        
+  render(){
+    return(
+      <View>
+        <Text style={{ fontSize:25 }}>Axios Api handler</Text>
       </View>
     )
   }
 }
-
-export default App;
+export default App
