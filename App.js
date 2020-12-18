b680b34b547010b21f6216309c9f1b53fbbb4402
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-18 15:51:21 +0530

    axios-3

--- App.js
+++ App.js
@@ -1,24 +1,65 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import axios from 'axios'
+import React from 'react';
+import {Text,FlatList,View, StyleSheet } from 'react-native';
+
+import axios from 'axios';
 
 class App extends React.Component {
+    constructor()
+    {
+      super ()
+      this.state = {
+        nameList:[],
+        isLoading:true,     
+       }
+    }
 
-  componentWillUnmount()
-  {
-    axios.get('https://reactnative.dev/movies.json')
-  .then(function (response) {
-    console.warn(response.data.movies);
-   })
-  
-  }
+    async componentDidMount(){
+        const  response = await axios.get('https://reactnative.dev/movies.json');
+        const item = await response.data.movies;
+        this.setState({nameList:item})
+        console.log(this.state.nameList)
+        console.log(this.state.isLoading)
+        console.log(this.state.nameList.length)
+        
+    }
 
-  render(){
-    return(
-      <View>
-        <Text style={{ fontSize:25 }}>Axios Api handler</Text>
-      </View>
-    )
-  }
+    render() {
+        const { nameList, isLoading } = this.state;
+        return (
+           
+            <View styles={styles.container}>
+            {isLoading?(
+                <FlatList
+                data={nameList}
+                keyExtractor={({ id }) => id}
+                renderItem={({ item }) => (
+                    
+                   
+                    <Text style= {styles.text}> Name :  {item.title},    ReleaseYear:  {item.releaseYear}</Text>
+                )}
+                />
+                ) :<Text>DATA is Loading</Text>
+            }
+            </View>
+          
+     
+        );
+    }
 }
-export default App
+
+export default App;
+const styles=StyleSheet.create({
+    container:{
+        padding:20,
+        flex: 1,
+        
+    },
+    text:{
+      fontSize:20,
+      margin:20,
+      backgroundColor:'#000',
+      color:'#ff0',
+      fontWeight:'bold'
+    }
+    
+});
