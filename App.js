d3b50475f90721985375a2d40b62ca0f29e6e468
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-22 10:22:56 +0530

    multi

--- App.js
+++ App.js
@@ -1,53 +1,84 @@
-i
-import React from 'react';
-import { StyleSheet , Text, View, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+
+import {StyleSheet, View, TouchableOpacity, Text} from 'react-native';
+
 import axios from 'axios';
-import { ScrollView } from 'react-native-gesture-handler'
+import { ScrollView } from 'react-native-gesture-handler';
 
-export default class App extends Comment {
+class App extends Component {
+    state={
+        data:'',
+        post:'',
+        multi1:'',
+        isloading:true,
+    }
+   
 
-  state={
-    data:'',
-    post:'',
-    isLoading: true,
-  }
-
-  getData = () => {
+   multipleRequestsInSingleCall = () => {
+       //const data='';
     axios
-    .get('https://reactnative.dev/movies.json')
-    .then(function(response){
-      alert(JSON.stringify(response.data));
-    })
-    .catch(function (error){
-      alert(error.message);
-    })
-    .finally(function(){
-      alert('finally called')
-    });
-  }
-  render(){
-    return(
-      <ScrollView>
-      <View style={styles.container}>
-        <Text style= {{ fontSize: 25, textAlign:'center'}}> Axios</Text>
-      </View>
+      .all([
+        axios
+          .get('https://jsonplaceholder.typicode.com/posts/1')
+          .then(function (response) {
+              //data=JSON.stringify(response.data);
+            console.log('Post 1 : ' + JSON.stringify(response.data));
+            
+          }),
+        axios
+          .get('https://jsonplaceholder.typicode.com/posts/2')
+          .then(function (response) {
+            //alert('Post 2 : ' + JSON.stringify(response.data));
+          }),
+      ])
+      .then(
+        axios.spread(function (acct, perms) {
+        alert('Both requests are now complete');
+        }),
+      );
+    //  this.setState({multi1:data});
+  };
+render(){
+  return (
+  <ScrollView>
+    <View style={styles.container}>
+      <Text style={{fontSize: 30, textAlign: 'center'}}>
+     Axio 
+      </Text>
+     
+      
 
-      <TouchableOpacity 
-       onPress={this.getData}
-       >
-       
-        <Text>Get</Text>
-       
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={this.multipleRequestsInSingleCall}>
+        <Text style={{fontSize:25, fontWeight:'bold' }}>Multiple Concurrent Requests In Single Call</Text>
       </TouchableOpacity>
-      </ScrollView>
 
-    );
-  }
+      <Text style={{textAlign: 'center', marginTop: 18}}>
+            {this.state.data} 
+      </Text>
+      <Text style={{textAlign: 'center', marginTop: 18,margin:30,}}>
+            {this.state.post} 
+      </Text>
+    </View>
+    </ScrollView>
+  );
 }
+};
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'pink'
-  }
-})
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+    padding: 16,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    backgroundColor: '#00F',
+    padding: 10,
+    width: '100%',
+    marginTop: 16,
+  },
+});
+
+export default App;
