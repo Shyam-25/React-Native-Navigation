113670fb0f229040ce47534aabc87019cd3cc18a
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-30 12:48:23 +0530

    first commit

--- App.js
+++ App.js
@@ -1,114 +1,24 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
- 
-import Video from 'react-native-video';
+import React, { Component } from 'react';
+import { Text, View,TouchableOpacity } from 'react-native';
+import VideoRecorder from 'react-native-beautiful-video-recorder';
 
-import MediaControls, { PLAYER_STATES } from 'react-native-media-controls';
-class App extends Component {
-  videoPlayer;
-constructor(props) {
-    super(props);
-    this.state = {
-      currentTime: 0,
-      duration: 0,
-      isFullScreen: false,
-      isLoading: true,
-      paused: false,
-      playerState: PLAYER_STATES.PLAYING,
-      screenType:'content'
-};
+ class App extends Component {
+  start = () => {
+  // 30 seconds
+    this.videoRecorder.open({ maxLength: 30 },(data) => {
+      console.log('captured data', data);
+    });
   }
-  onSeek = seek => {
-          this.videoPlayer.seek(seek);
-        };
-  onPaused = playerState => {
-          this.setState({
-            paused: !this.state.paused,
-            playerState,
-          });
-        };
-  onReplay = () => {
-          this.setState({ playerState: PLAYER_STATES.PLAYING });
-          this.videoPlayer.seek(0);
-        };
-  onProgress = data => {
-          const { isLoading, playerState } = this.state;
-          
-          if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
-            this.setState({ currentTime: data.currentTime });
-          }
-        };
-  onLoad = data => this.setState({ duration: data.duration, isLoading: false });
-  onLoadStart = data => this.setState({ isLoading: true });
-  onEnd = () => this.setState({ playerState: PLAYER_STATES.ENDED });
-  onError = () => alert('Oh! ', error);
-  exitFullScreen = () => {
-          alert("Exit  screen");
-        };
-  enterFullScreen = () => {};
-  onFullScreen = () => {
-          if(this.state.screenType=='content')
-            this.setState({screenType:'cover'});
-         else
-            this.setState({screenType:'content'});
-        };
-  renderToolbar = () => (
-          <View >
-              <Text> toolbar </Text>
-          </View>
-        );
-  onSeeking = currentTime => this.setState({ currentTime });
-        
-   render() {
-     return (
-      <View style={styles.container}>
-        <Video
-          onEnd={this.onEnd}
-          onLoad={this.onLoad}
-          onLoadStart={this.onLoadStart}
-          onProgress={this.onProgress}
-          paused={this.state.paused}
-          ref={videoPlayer => (this.videoPlayer = videoPlayer)}
-          resizeMode={this.state.screenType}
-          onFullScreen={this.state.isFullScreen}
-          source={ {require('file:///C:/Users/Shyam%20Sundar/Downloads/tom_and_jerry_31.mp4')}} 
-          style={styles.mediaPlayer}
-          volume={10}
-        />
-        <MediaControls
-          duration={this.state.duration}
-          isLoading={this.state.isLoading}
-          mainColor="#333"
-          onFullScreen={this.onFullScreen}
-          onPaused={this.onPaused}
-          onReplay={this.onReplay}
-          onSeek={this.onSeek}
-          onSeeking={this.onSeeking}
-          playerState={this.state.playerState}
-          progress={this.state.currentTime}
-          toolbar={this.renderToolbar()}
-        />
+  
+  render() {
+    return (
+      <View>
+        <TouchableOpacity onPress={this.start}>
+          <Text>Start</Text>
+        </TouchableOpacity>
+        <VideoRecorder ref={(ref) => { this.videoRecorder = ref; }} />
       </View>
     );
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  toolbar: {
-    marginTop: 30,
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 5,
-  },
-  mediaPlayer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    backgroundColor: 'black',
-  },
-});
 export default App;
