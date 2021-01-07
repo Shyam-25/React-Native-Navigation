e194ff55cdc7f9058bc63c7bf3464e16bf722643
Shyam-25 <shyam.s@optisolbusiness.com>
2021-01-07 13:31:09 +0530

    UI

--- App.js
+++ App.js
@@ -1,80 +1,58 @@
-import React, { useState,useEffect} from 'react';
-import{Button,StyleSheet,TextInput,View,Text} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { Component } from 'react';
+import { StyleSheet,Text, TextInput, View, Button, TouchableOpacity } from 'react-native';
 
- const STORAGE_KEY = '@save_name'
- const App =()=> {
- 
-    const [name, setName] = useState('')
-    const saveData = async () => {
-        try {
-          await AsyncStorage.setItem(STORAGE_KEY, name)
-          alert('Data successfully saved')
-        } catch (e) {
-          alert('Failed to save the data to the storage')
-        }
-      }
-    const readData = async () => {
-        try {
-          const userName = await AsyncStorage.getItem(STORAGE_KEY)
-      
-          if (userName !== null) {
-            setName(userName)
-          }
-        } catch (e) {
-          alert('Failed to fetch the data from storage')
-        }
-    }
-    const clearStorage = async () => {
-        try {
-          await AsyncStorage.clear()
-          alert('Storage successfully cleared!')
-        } catch (e) {
-          alert('Failed to clear the async storage.')
-        }
-    }
-    const onChangeText = userName => setName(userName)
-
-    const onSubmitEditing = () => {
-        if (!name) return 
-        saveData(name)
-       
-        setName('')
-    }
-    useEffect(() => {
-    readData()
-    }, [])
+export default class App extends Component{
+  constructor(props){
+    super(props);
+     this.state={
+       morning:'',
+       afternoon:''
+     }
+  }
+  render(){
     return(
-      <View style={styles.container}>
-          <TextInput style={styles.textInput} value={name}
-          placeholder='Enter your Name' onChangeText={onChangeText}
-          onSubmitEditing={onSubmitEditing}/>
-          <Button title='Clear' onPress={clearStorage}/>
-          <Text style={styles.text}>Your Name is {name}</Text>
-      </View>
-    );
+      
+      <View style ={styles.container}>
+      <TouchableOpacity>
 
-};
+        <Text style={styles.text}>Half Day</Text>
+     
+      </TouchableOpacity>
+      <View style={styles.textview}>
+        <Text style={styles.textday}>Morning</Text>
+        <Text style={styles.textday}>Afternoon</Text>
+      </View>
+      </View>
+
+    )
+  }
+}
 
 const styles= StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        padding:20,
-        backgroundColor: 'pink'
-    },
-    textInput:{
-        borderColor:'black',
-        borderWidth:1,
-        width:150,
-        marginBottom:30,
-    },
-    text: {
-        fontSize: 24,
-        padding: 10,
-        backgroundColor: '#fff',
-        fontWeight:'bold'
-    },
+  container:{
+    flex:1,
+    backgroundColor:'pink'
+  },
+  text:{
+    fontSize:20,
+    fontWeight:'bold',
+    marginTop:150,
+    justifyContent:'center',
+    alignItems:'center',
+    marginLeft:60
+    
+  },
+  textview:{
+    
+    // alignItems:'center',
+    // justifyContent:'center'
+    marginTop:50
+
+  },
+  textday:{
+    fontSize:20,
+    marginLeft:80,
+    marginTop:10
+  }
 })
-export default App;
+
